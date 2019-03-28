115b40ba92ce6703c113e2c7c6a9441d7cee7b5d
parsa Honarmand <parsa.Honarmand@gmail.com>
2019-03-27 20:03:12 -0600

    New Changes made to PayGUI, Materials created

--- src/librarysystem/gui/Materials.java
+++ src/librarysystem/gui/Materials.java
@@ -32,33 +32,26 @@ import javax.swing.SwingConstants;
 import java.awt.Panel;
 import javax.swing.JToolBar;
 
-public class PayGUI extends JPanel {
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import librarysystem.LibrarySystem;
+
+public class Materials extends JPanel {
+	
 	private LibrarySystem librarySystem;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	
-	private User currentUser = librarySystem.getUserManager().getCurrentUser();
-	private double fee = currentUser.getOverdueFee();
-	private String feeString = Double.toString(fee);
-	private JLabel lblOutStandingFees = new JLabel(feeString);
-	
 
 	/**
 	 * Create the panel.
 	 */
-	public PayGUI(LibrarySystem librarySystem) {
-		this();
-		
-		this.librarySystem = librarySystem;
-		librarySystem.updateGUI(this);
-
-	}
-
-	public PayGUI() {
+	public Materials() {
 		this.setBackground(Color.WHITE);
 		this.setBounds(0, 0, 1075, 747);
 		
@@ -148,122 +141,39 @@ public class PayGUI extends JPanel {
 					.addContainerGap())
 		);
 		
-		JLabel lblOutstandingFees = new JLabel("Outstanding Fees:");
+		JLabel lblNewLabel = new JLabel("Current Materials");
 		
-		JLabel lblCard = new JLabel("Card #");
-		
-		JLabel lblExpiryDate = new JLabel("Expiry date;");
-		
-		JLabel lblCvc = new JLabel("CVC:");
-		
-		JLabel lblFirstName = new JLabel("First name:");
-		
-		JLabel lblLastName = new JLabel("Last name:");
-		
-
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		
-		
-		JLabel lblError = new JLabel("Please enter all your info");
-		lblError.setVisible(false);
-		
-		JButton btnPay = new JButton("Pay");
-		btnPay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (!(textField_1.getText() != "") && !(textField_2.getText() != "") && !(textField_3.getText() != "") && !(textField_4.getText() != "") && !(textField_5.getText() != "")) {
-					currentUser.setOverdueFee(0.00);
-					lblOutStandingFees.setText("0.00");
-					lblError.setText("Thank you!");
-					lblError.setVisible(true);
-				}
-				else if (currentUser.getOverdueFee() == 0.0) {
-					lblError.setText("You have no outstanding Fees!");
-					lblError.setVisible(true);
-				}
-				else {
-					lblError.setText("Please enter all your info!");
-					lblError.setVisible(true);
-				}
-			}
-		});
-
+		JLabel lblOnHold = new JLabel("On Hold");
 		GroupLayout gl_profilePanel = new GroupLayout(profilePanel);
 		gl_profilePanel.setHorizontalGroup(
 			gl_profilePanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_profilePanel.createSequentialGroup()
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_profilePanel.createSequentialGroup()
-							.addGap(90)
-							.addGroup(gl_profilePanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblLastName)
-								.addComponent(lblFirstName)
-								.addComponent(lblCvc)
-								.addComponent(lblExpiryDate)
-								.addComponent(lblCard)
-								.addComponent(lblOutstandingFees))
-							.addGap(76)
-							.addGroup(gl_profilePanel.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(lblOutStandingFees)
-								.addComponent(textField_1)
-								.addComponent(textField_2)
-								.addComponent(textField_3)
-								.addComponent(textField_4)
-								.addComponent(textField_5)
-								.addComponent(lblError, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-						.addGroup(gl_profilePanel.createSequentialGroup()
-							.addGap(308)
-							.addComponent(btnPay)))
-					.addContainerGap(416, Short.MAX_VALUE))
+					.addGap(156)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.RELATED, 270, Short.MAX_VALUE)
+					.addComponent(lblOnHold)
+					.addGap(256))
 		);
 		gl_profilePanel.setVerticalGroup(
 			gl_profilePanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_profilePanel.createSequentialGroup()
-					.addGap(64)
+					.addGap(61)
 					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblOutstandingFees)
-						.addComponent(lblOutStandingFees))
-					.addGap(18)
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblCard)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblExpiryDate)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblCvc)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblFirstName)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_profilePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblLastName)
-						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addComponent(btnPay)
-					.addGap(18)
-					.addComponent(lblError)
-					.addContainerGap(313, Short.MAX_VALUE))
+						.addComponent(lblNewLabel)
+						.addComponent(lblOnHold))
+					.addContainerGap(617, Short.MAX_VALUE))
 		);
 		profilePanel.setLayout(gl_profilePanel);
 		setLayout(groupLayout);
-		
+	}
 
+	
+	public Materials(LibrarySystem librarySystem) {
+		this();
+		
+		this.librarySystem = librarySystem;
+		librarySystem.updateGUI(this);
+		
 	}
 }
+
