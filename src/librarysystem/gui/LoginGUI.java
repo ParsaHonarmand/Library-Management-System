b91ce0d788ebf13801c45350ab38d67ff0e5c9d5
Rory <roryskipper123@gmail.com>
2019-04-10 14:53:09 -0600

    Added check for max materials being held and taken out

--- src/librarysystem/gui/LoginGUI.java
+++ src/librarysystem/gui/LoginGUI.java
@@ -9,14 +9,15 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class LoginGUI extends JPanel {
-	
+
 	private final LibrarySystem librarySystem;
-	
+
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 
 	/**
 	 * Create the panel.
+	 * 
 	 * @param librarySystem The system to base the GUI on
 	 */
 	public LoginGUI(LibrarySystem librarySystem) {
@@ -25,45 +26,46 @@ public class LoginGUI extends JPanel {
 
 		this.librarySystem.updateGUI(this);
 		setLayout(null);
-		
+
 		/**
-		 * Setting panel characteristics 
+		 * Setting panel characteristics
 		 */
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(245, 245, 245));
 		panel.setBounds(259, 230, 724, 315);
 		add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblBanner = new JLabel("");
 		lblBanner.setBounds(15, 0, 1250, 200);
 		lblBanner.setIcon(new ImageIcon("resources/banner_img.png"));
-		setLayout(null); 
+		setLayout(null);
 		this.add(lblBanner);
-		
+
 		/**
-		 * Labels used to display information to user in order to notify of actions performed or data input requirments 
+		 * Labels used to display information to user in order to notify of actions
+		 * performed or data input requirments
 		 */
 		JLabel lblUsername = new JLabel("Username:");
 		lblUsername.setBounds(110, 80, 105, 29);
 		panel.add(lblUsername);
 		lblUsername.setForeground(new Color(0, 102, 204));
 		lblUsername.setFont(new Font("Times New Roman", Font.PLAIN, 24));
-		
+
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setBounds(116, 147, 110, 44);
 		panel.add(lblPassword);
 		lblPassword.setForeground(new Color(0, 102, 204));
 		lblPassword.setFont(new Font("Times New Roman", Font.PLAIN, 24));
-		
+
 		JLabel lblRegister = new JLabel("No account ? Register Now !");
 		lblRegister.setBounds(141, 226, 267, 32);
 		panel.add(lblRegister);
 		lblRegister.setForeground(new Color(51, 102, 102));
 		lblRegister.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-		
+
 		/**
-		 * Textfeilds that allow for input from user
+		 * Textfilds that allow for input from user
 		 */
 		usernameField = new JTextField();
 		usernameField.setHorizontalAlignment(SwingConstants.CENTER);
@@ -74,7 +76,7 @@ public class LoginGUI extends JPanel {
 		usernameField.setMaximumSize(dimension);
 		usernameField.setFont(new Font("Times New Roman", Font.PLAIN, 17));
 		usernameField.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
 		passwordField.setForeground(new Color(51, 102, 102));
@@ -83,7 +85,7 @@ public class LoginGUI extends JPanel {
 		panel.add(passwordField);
 		passwordField.setMinimumSize(dimension);
 		passwordField.setMaximumSize(dimension);
-		
+
 		/**
 		 * Used to login in to the system and verification validity of user
 		 */
@@ -96,16 +98,19 @@ public class LoginGUI extends JPanel {
 			public void mouseReleased(MouseEvent e) {
 				User user = librarySystem.getUserManager().login(usernameField.getText(), passwordField.getText());
 				if (user == null) {
-					JOptionPane.showMessageDialog(librarySystem.getFrame(), "Username and password are not valid. Try again.");
-				} if (user.getOverdueFee()>50) {
-					librarySystem.updateGUI(new OverdueGUI(librarySystem));
+					JOptionPane.showMessageDialog(librarySystem.getFrame(),
+							"Username and password are not valid. Try again.");
 				} else {
-					librarySystem.updateGUI(new HomeGUI(librarySystem));
+					if (user.getOverdueFee() > 50) {
+						librarySystem.updateGUI(new OverdueGUI(librarySystem));
+					} else {
+						librarySystem.updateGUI(new HomeGUI(librarySystem));
+					}
 				}
 			}
 		});
 		btnLogin.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		
+
 		/**
 		 * Used to insert image on to panel
 		 */
@@ -113,7 +118,6 @@ public class LoginGUI extends JPanel {
 		bckgGifImg.setBounds(0, 0, librarySystem.WIDTH, librarySystem.HEIGHT);
 		add(bckgGifImg);
 		bckgGifImg.setIcon(new ImageIcon("resources/libwall.jpg"));
-		
 
 		this.librarySystem.updateGUI(this);
 	}
